fb234df708bbbde07a83d7c03d69f74e26cd7519
Christoph Ciesla <christoph.ciesla@mytum.de>
2022-03-12 22:11:01 +0100

    Add basic XScreensaver support

--- source/locker/lock_session.cpp
+++ source/locker/lock_session.cpp
@@ -11,25 +11,25 @@ namespace locker
 namespace
 {
 #ifndef WIN32
-struct LockMethod
+struct DBusLockMethod
 {
     QString service{};
     QString path{};
     QString interface{};
 };
 
-ErrorCode LockSessionImpl_linux()
+ErrorCode LockSessionImpl_DBus()
 {
     QDBusConnection bus = QDBusConnection::sessionBus();
     if(!bus.isConnected())
     {
         return ErrorCode::SystemInaccessible;
     }
-    QVector<LockMethod> lock_methods{
-        LockMethod{"org.gnome.ScreenSaver", "/org/gnome/ScreenSaver", "org.gnome.ScreenSaver"},
-        LockMethod{"org.mate.ScreenSaver", "/", "org.mate.ScreenSaver"},
-        LockMethod{"org.xfce.ScreenSaver", "/", "org.xfce.Screensaver"},
-        LockMethod{"org.freedesktop.ScreenSaver", "/org/freedesktop/ScreenSaver", "org.freedesktop.ScreenSaver"}
+    QVector<DBusLockMethod> lock_methods{
+        DBusLockMethod{"org.gnome.ScreenSaver", "/org/gnome/ScreenSaver", "org.gnome.ScreenSaver"},
+        DBusLockMethod{"org.mate.ScreenSaver", "/", "org.mate.ScreenSaver"},
+        DBusLockMethod{"org.xfce.ScreenSaver", "/", "org.xfce.Screensaver"},
+        DBusLockMethod{"org.freedesktop.ScreenSaver", "/org/freedesktop/ScreenSaver", "org.freedesktop.ScreenSaver"}
     };
 
     for(const auto& lock_method: lock_methods)
@@ -44,6 +44,31 @@ ErrorCode LockSessionImpl_linux()
     }
     return ErrorCode::DesktopEnvironmentNotSupported;
 }
+
+ErrorCode LockSessionImpl_XScreenSaver()
+{
+    const int return_value{QProcess::execute("xscreensaver-command", {"-lock"})};
+    if (return_value >= 0)
+    {
+        return ErrorCode::Success;
+    }
+    return ErrorCode::DesktopEnvironmentNotSupported;
+}
+
+ErrorCode LockSessionImpl_Linux()
+{
+    const ErrorCode error_dbus{LockSessionImpl_DBus()};
+    if (error_dbus == ErrorCode::Success)
+    {
+        return ErrorCode::Success;
+    }
+    const ErrorCode error_xscreensaver{LockSessionImpl_XScreenSaver()};
+    if (error_xscreensaver == ErrorCode::Success)
+    {
+        return ErrorCode::Success;
+    }
+    return std::max(error_dbus, error_xscreensaver);
+}
 #endif
 }
 
@@ -53,7 +78,7 @@ ErrorCode LockSession()
     ::LockWorkStation();
     return ErrorCode::Success;
 #else
-    return LockSessionImpl_linux();
+    return LockSessionImpl_Linux();
 #endif
 }
 }
